bd7fe5224bb54b6f67220d8ebd3a5f0495abbb39
Anyeseu Oh <anyeseu2016@gmail.com>
2023-06-21 19:32:36 +0900

    스크립트 기능 수정
    - 마지막 스크립트 간헐적으로 넘어가는 현상 수정

--- Assets/Scripts/Puzzle/ToggleManager.cs
+++ Assets/Scripts/Puzzle/ToggleManager.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ToggleManager : MonoBehaviour
 {
+    public ToggleGame toggleGame;
+    public ActionFuntion actionFuntion;
+
+    [Header("연결 필수")]
+    public GameObject togglePanel;
+    public GameObject clearPanel;
 
     public Image img1;
     public Image img2;
@@ -12,13 +18,11 @@ public class ToggleManager : MonoBehaviour
     public Image img4;
     public Image img5;
 
-    public ToggleGame toggleGame;
-
     public bool isClear = false;
 
     void Start()
     {
-        
+        actionFuntion = GameObject.Find("ActionFunction").GetComponent<ActionFuntion>();
     }
 
     
@@ -29,7 +33,9 @@ public class ToggleManager : MonoBehaviour
         {
             isClear = true;
             Debug.Log("CLEAR!!!!!!!!!!!!!!!!");
-            return;
+            StartCoroutine(WaitClearFunction());
+            actionFuntion.RestartGame();
+            enabled = false;
         }
     }
 
@@ -37,24 +43,43 @@ public class ToggleManager : MonoBehaviour
     {
         img1.color = Color.white;
         toggleGame = img1.transform.GetComponentInParent<ToggleGame>();
-        toggleGame.flag1 = false;
+        toggleGame.thisFlag = false;
 
         img2.color = Color.white;
         toggleGame = img2.transform.GetComponentInParent<ToggleGame>();
-        toggleGame.flag1 = false;
+        toggleGame.thisFlag = false;
 
         img3.color = Color.white;
         toggleGame = img3.transform.GetComponentInParent<ToggleGame>();
-        toggleGame.flag1 = false;
+        toggleGame.thisFlag = false;
 
         img4.color = Color.white;
         toggleGame = img4.transform.GetComponentInParent<ToggleGame>();
-        toggleGame.flag1 = false;
+        toggleGame.thisFlag = false;
 
         img5.color = Color.white;
         toggleGame = img5.transform.GetComponentInParent<ToggleGame>();
-        toggleGame.flag1 = false;
+        toggleGame.thisFlag = false;
 
 
+    }
+
+    public IEnumerator WaitClearFunction()
+    {
+        yield return StartCoroutine(ClearToggleGame());
+    }
+
+    public IEnumerator ClearToggleGame()
+    {
+        yield return new WaitForSeconds(1f);
+        clearPanel.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        togglePanel.SetActive(false);
+        clearPanel.SetActive(false);
+    }
+
+    public void PlayToggleGame()
+    {
+        togglePanel.SetActive(true);
     }
 }
