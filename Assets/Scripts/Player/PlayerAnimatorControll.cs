88b103092f44af4505f147989272c951a46a4dfb
songzealot <gjggjggjg123@naver.com>
2023-06-26 11:01:38 +0900

    20230626-02 애니메이션 관련
    애니메이션 관련 코드 정리

--- Assets/Scripts/Player/PlayerAnimatorControll.cs
+++ Assets/Scripts/Player/PlayerAnimatorControll.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class PlayerAnimatorControll : MonoBehaviour
 {
     // 모델에 달려있는 애니메이터
-    Animator animator;
+    private Animator animator;
 
     // 애니메이션 목록
     internal enum Animation_State
@@ -26,7 +27,9 @@ public class PlayerAnimatorControll : MonoBehaviour
     // 인스펙터
     [Header("애니메이션")]
     [Tooltip("애니메이션 상태")]
-    [SerializeField,ReadOnly] Animation_State animationState;
+    [SerializeField] private Animation_State animationState;
+
+
 
     private void Awake()
     {
@@ -111,5 +114,108 @@ public class PlayerAnimatorControll : MonoBehaviour
     internal void SetAnimationState(Animation_State state)
     {
         animationState = state;
+    }
+
+    /// <summary>
+    /// 현재 상태의 애니메이션 재생시간 반환
+    /// </summary>
+    /// <param name="state">재생시간을 반환받을 애니메이션 상태</param>
+    /// <returns>애니메이션 재생 시간</returns>
+    internal float GetAnimationDurationTime(Animation_State state)
+    {
+        // 재생시간
+        float time = 0;
+        // 상태머신 state를 비교하기 위한 변수
+        string smState = GetStringFromAnimationStateMachine(state);
+        // 상태머신 state의 애니메이션 clip
+        string smClip = "";
+        // 상태머신 state의 배속
+        float smSpeed = 1;
+
+        // 상태머신 관련 초기화
+        RuntimeAnimatorController rac = animator.runtimeAnimatorController;
+        AnimatorController ac = animator.runtimeAnimatorController as AnimatorController;
+        AnimatorStateMachine sm = ac.layers[0].stateMachine;
+
+        float skillLoopTime = 1;
+
+        // 현재 상태의 애니메이션 찾고 재생시간 반환
+        // 상태와 연결된 애니메이션 이름과 상태 재생속도, 상태 반복 횟수 확인
+        for (int i = 0; i < sm.states.Length; i++)
+        {
+            if (sm.states[i].state.name.Equals(smState))
+            {
+                if (smState.Equals("Skill"))
+                {
+                    skillLoopTime = sm.states[i].state.transitions[0].exitTime;
+                }
+                smClip = sm.states[i].state.motion.name;
+                smSpeed = sm.states[i].state.speed;
+            }
+        }
+
+        // 찾은 애니메이션 이름으로 애니메이션 재생 길이 확인
+        for (int i = 0; i < rac.animationClips.Length; i++)
+        {
+            if (rac.animationClips[i].name.Equals(smClip))
+            {
+                time = rac.animationClips[i].length;
+            }
+        }
+
+        // 애니메이션 재생 길이, 상태 재생 속도, 반복 횟수를 계산한 최종 상태 재생 길이 반환
+        if (smState.Equals("Skill"))
+        {
+            return (time / smSpeed) * skillLoopTime;
+        }
+        else
+        {
+            return time / smSpeed;
+        }
+    }
+
+    /// <summary>
+    /// 애니메이션 상태에 해당하는 상태 머신의 이름을 string으로 반환
+    /// </summary>
+    /// <param name="state">string을 반환할 애니메이션 상태</param>
+    /// <returns></returns>
+    string GetStringFromAnimationStateMachine(Animation_State state)
+    {
+        string smStateString = "";
+
+        // 성능을 위해 하드코딩
+        switch (state)
+        {
+            case Animation_State.Move:
+                smStateString = "Move";
+                break;
+            case Animation_State.Walk:
+                
+                break;
+            case Animation_State.Jump:
+                smStateString = "Jump";
+                break;
+            case Animation_State.Attack1:
+                smStateString = "Attack1";
+                break;
+            case Animation_State.Attack2:
+                smStateString = "Attack2";
+                break;
+            case Animation_State.Attack3:
+                smStateString = "Attack3";
+                break;
+            case Animation_State.Hit:
+                smStateString = "Hit";
+                break;
+            case Animation_State.Avoid1:
+                smStateString = "Roll";
+                break;
+            case Animation_State.Avoid2:
+                break;
+            case Animation_State.Dead:
+                smStateString = "Dead";
+                break;
+        }
+        return smStateString;
     }
 }
