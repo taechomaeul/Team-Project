3f249d9f8386b0a890db3df0ef46eded8acd3da4
songzealot <gjggjggjg123@naver.com>
2023-06-27 15:05:12 +0900

    20230627-02 세이브 관련
    멤버 접근 함수 추가
    불러온 데이터 적용 함수 배치

--- Assets/Scripts/Test/SaveTest.cs
+++ Assets/Scripts/Test/SaveTest.cs
@@ -5,7 +5,6 @@ public class SaveTest : MonoBehaviour
     private void Start()
     {
         SaveManager.Instance.SaveCurrentData(1);
-        StartCoroutine(SaveManager.Instance.Test());
         //SaveManager.Instance.LoadSaveData();
     }
 }
