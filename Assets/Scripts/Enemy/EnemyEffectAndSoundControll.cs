c560711045b01dcaee07202a85b03b413b4ef49e
songzealot <gjggjggjg123@naver.com>
2023-06-28 15:58:15 +0900

    20230628-03 이펙트 관련
    적 이펙트 표현 구조 수정

--- Assets/Scripts/Enemy/EnemyEffectAndSoundControll.cs
+++ Assets/Scripts/Enemy/EnemyEffectAndSoundControll.cs
@@ -1,12 +1,8 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.Audio;
 
 public class EnemyEffectAndSoundControll : MonoBehaviour
 {
-    AudioMixer audioMixer;
-
-    [Header("이펙트 오브젝트")]
+    [Header("재생할 이펙트 오브젝트")]
     [Tooltip("공격1")]
     [SerializeField] private GameObject effectAttack1;
 
@@ -19,81 +15,85 @@ public class EnemyEffectAndSoundControll : MonoBehaviour
     [Tooltip("스킬")]
     [SerializeField] private GameObject effectSkill;
 
-    [Header("이펙트 선딜레이")]
-    [Tooltip("공격1")]
-    [SerializeField] private float delayAttack1;
+    //[Header("이펙트 선딜레이")]
+    //[Tooltip("공격1")]
+    //[SerializeField] private float delayAttack1;
 
-    [Tooltip("공격2")]
-    [SerializeField] private float delayAttack2;
+    //[Tooltip("공격2")]
+    //[SerializeField] private float delayAttack2;
 
-    [Tooltip("공격3")]
-    [SerializeField] private float delayAttack3;
+    //[Tooltip("공격3")]
+    //[SerializeField] private float delayAttack3;
 
-    [Tooltip("스킬")]
-    [SerializeField] private float delaySkill;
+    //[Tooltip("스킬")]
+    //[SerializeField] private float delaySkill;
 
-    [Header("공격 사운드")]
-    [Tooltip("공격1")]
-    [SerializeField] private AudioClip soundAttack1;
+    //[Header("사운드")]
+    //[Tooltip("효과음 재생")]
+    //[SerializeField]AudioSource audioSource;
 
-    [Tooltip("공격2")]
-    [SerializeField] private AudioClip soundAttack2;
+    //[Header("공격 사운드")]
+    //[Tooltip("공격1")]
+    //[SerializeField] private AudioClip soundAttack1;
 
-    [Tooltip("공격3")]
-    [SerializeField] private AudioClip soundAttack3;
+    //[Tooltip("공격2")]
+    //[SerializeField] private AudioClip soundAttack2;
 
-    [Tooltip("스킬")]
-    [SerializeField] private AudioClip soundSkill;
+    //[Tooltip("공격3")]
+    //[SerializeField] private AudioClip soundAttack3;
+
+    //[Tooltip("스킬")]
+    //[SerializeField] private AudioClip soundSkill;
 
 
-
-    private void Awake()
-    {
-    }
 
     internal void TrunOnEffectAttack(int attackType)
     {
         switch (attackType)
         {
             case 0:
-                StartCoroutine(Attack1());
+                //StartCoroutine(Attack1());
+                effectAttack1.SetActive(true);
                 break;
             case 1:
-                StartCoroutine(Attack2());
+                //StartCoroutine(Attack2());
+                effectAttack2.SetActive(true);
                 break;
             case 2:
-                StartCoroutine(Attack3());
+                //StartCoroutine(Attack3());
+                effectAttack3.SetActive(true);
                 break;
         }
     }
 
     internal void TrunOnEffectSkill()
     {
-        StartCoroutine(Skill());
-    }
-
-    private IEnumerator Attack1()
-    {
-        yield return new WaitForSeconds(delayAttack1);
-        effectAttack1.SetActive(true);
-
-    }
-
-    private IEnumerator Attack2()
-    {
-        yield return new WaitForSeconds(delayAttack2);
-        effectAttack2.SetActive(true);
-    }
-
-    private IEnumerator Attack3()
-    {
-        yield return new WaitForSeconds(delayAttack3);
-        effectAttack3.SetActive(true);
-    }
-
-    private IEnumerator Skill()
-    {
-        yield return new WaitForSeconds(delaySkill);
+        //StartCoroutine(Skill());
         effectSkill.SetActive(true);
     }
+
+    //private IEnumerator Attack1()
+    //{
+    //    yield return new WaitForSeconds(delayAttack1);
+    //    effectAttack1.SetActive(true);
+
+    //}
+
+    //private IEnumerator Attack2()
+    //{
+    //    yield return new WaitForSeconds(delayAttack2);
+    //    effectAttack2.SetActive(true);
+    //}
+
+    //private IEnumerator Attack3()
+    //{
+    //    yield return new WaitForSeconds(delayAttack3);
+    //    effectAttack3.SetActive(true);
+    //}
+
+    //private IEnumerator Skill()
+    //{
+    //    yield return new WaitForSeconds(delaySkill);
+    //    effectSkill.SetActive(true);
+    //}
 }
