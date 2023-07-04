181dd3d470fba3a1732d9ec6eeca6578dac6dabe
songzealot <gjggjggjg123@naver.com>
2023-07-04 15:16:58 +0900

    20230704-04 카메라 관련
    카메라 거리 조절 코드 주석 추가

--- Assets/Scripts/Common/CameraDistanceControll.cs
+++ Assets/Scripts/Common/CameraDistanceControll.cs
@@ -28,24 +28,31 @@ public class CameraDistanceControll : MonoBehaviour
 
     private void Start()
     {
+        // 초기 세팅
         originZ = transform.localPosition.z;
         cameraZposition = transform.localPosition.z;
     }
 
     private void FixedUpdate()
     {
+        // Ray 세팅
         Ray ray = new(transform.position, player.transform.position - transform.position);
+
+        // 카메라에서 플레이어에게 쏜 Ray가 레이어 오브젝트에 닿거나, 카메라 OverlapSphere 범위에 레이어 오브젝트가 충돌했다면
         if (Physics.Raycast(ray, out RaycastHit hit, Vector3.Distance(transform.position, player.transform.position), layerMask)
-    || Physics.OverlapSphereNonAlloc(transform.position, CameraCollisionDetectRadiusForDecrease, tempColliderArray, layerMask) != 0)
+        || Physics.OverlapSphereNonAlloc(transform.position, CameraCollisionDetectRadiusForDecrease, tempColliderArray, layerMask) != 0)
         {
+            // 카메라 거리 비율만큼 감소
             cameraZposition = Mathf.Lerp(cameraZposition, 0, changeRatio);
         }
+        // 카메라에서 플레이어에게 쏜 Ray가 레이어 오브젝트에 닿지 않고, 카메라 OverlapShpere 범위에 레이어 오브젝트가 충돌하지 않았다면
         if (!Physics.Raycast(ray, out hit, Vector3.Distance(transform.position, player.transform.position), layerMask)
         && Physics.OverlapSphereNonAlloc(transform.position, CameraCollisionDetectRadiusForIncrease, tempColliderArray, layerMask) == 0)
         {
+            // 카메라 거리 비율만큼 증가
             cameraZposition = Mathf.Lerp(cameraZposition, originZ, changeRatio);
         }
-
+        // 변경된 카메라 거리 적용
         transform.localPosition = new Vector3(0, 2, cameraZposition);
     }
 }
