58e2db987d2392dd86bf7a7c4c6605d1e0c9a265
songzealot <gjggjggjg123@naver.com>
2023-07-05 19:35:06 +0900

    20230705-09 스탯 관련
    기본 스탯 가져오는 코드 추가
    현재 호출 타이밍으로 인한 오류 발생으로 추가 수정 필요

---
+++ Assets/Scripts/Common/DefaultStatManager.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultStatManager : MonoBehaviour
+{
+    private static DefaultStatManager instance;
+    public static DefaultStatManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                return null;
+            }
+            return instance;
+        }
+    }
+
+    // 플레이어와 적 기본 스탯 데이터
+    private List<Dictionary<string, object>> statData;
+
+    // 인스펙터
+    [Header("CSV 파일")]
+    [Tooltip("경로")]
+    [SerializeField] string path;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        statData = CSVReader.Read(path);
+    }
+
+    /// <summary>
+    /// 플레이어 기본 스탯
+    /// </summary>
+    /// <returns>maxHp, currentHp, movingSpeed, attack</returns>
+    internal Dictionary<string, object> GetPlayerData()
+    {
+        return statData[0];
+    }
+
+    /// <summary>
+    /// 일반 몬스터 기본 스탯
+    /// </summary>
+    /// <returns>maxHp, currentHp, movingSpeed, attack, detectAngle, detectRadius, attackCycle, attackRange</returns>
+    internal Dictionary<string, object> GetEnemyData()
+    {
+        return statData[1];
+    }
+
+    /// <summary>
+    /// 중간 보스 기본 스탯
+    /// </summary>
+    /// <returns>
+    /// maxHp, currentHp, movingSpeed, attack, detectAngle, detectRadius, attackCycle, attackRange,
+    /// skillDamage, skillCoolDown, skillCastRange, skillPhaseHpRatio
+    /// </returns>
+    internal Dictionary<string, object> GetMiniBossData()
+    {
+        return statData[2];
+    }
+
+    /// <summary>
+    /// 최종 보스 기본 스탯
+    /// </summary>
+    /// <returns>
+    /// maxHp, currentHp, movingSpeed, attack, detectAngle, detectRadius, attackCycle, attackRange,
+    /// skillDamage, skillCoolDown, skillCastRange, skillPhaseHpRatio
+    /// </returns>
+    internal Dictionary<string, object> GetFinalBossData()
+    {
+        return statData[3];
+    }
+}
