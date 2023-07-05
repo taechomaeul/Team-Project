aa136c83528d69c2dcc8cae7cedf86920e8ba79c
Anyeseu Oh <anyeseu2016@gmail.com>
2023-07-05 13:16:03 +0900

    다국어지원에 따른 고정 텍스트 변경 코드 추가
    - 코드만 구성 (세팅 필요함)
    - 이름을 받아와서 검색하고 인덱스를 반환해서, 현재 언어 설정에 따라 텍스트를 변경하는 형식

---
+++ Assets/Scripts/Common/ChanageUIFloorManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChanageUIFloorManager : MonoBehaviour
+{
+
+    [Header("메인 패널 부분 변경 / F키 변경")]
+    public GameObject[] fPanels; //FCommonPanel_Search (처럼 묶여져 있는 패널을 직접 연결해야함)
+
+    [Header("일지 패널 부분 변경 / 버튼이름 및 내용 변경")]
+    public GameObject[] jPanels;
+
+    private ChangeLanguage changeLanguage;
+
+    void Start()
+    {
+        changeLanguage = GameObject.Find("ActionFunction").GetComponent<ChangeLanguage>();
+    }
+
+    public void ChangeAllPanel()
+    {
+        ChangeMainPanel();
+        ChangeJournalPanel();
+    }
+
+    public void ChangeMainPanel() // F패널용
+    {
+
+        for (int i = 0; i < fPanels.Length; i++) //저장된 데이터만큼
+        {
+            int index = changeLanguage.FindPanelNameOfIndex(fPanels[i].name);
+
+            Debug.Log($"Index : {index} / ChildCount : {fPanels[i].transform.childCount}");
+            for (int j = 0; j < fPanels[i].transform.childCount; j++) //3
+            {
+                string lang = SettingManager.Instance.GetCurrentLanguageIndexToString();
+                if (lang.Equals("KR"))
+                {
+                    fPanels[i].transform.GetChild(j).GetComponent<Text>().text = changeLanguage.langCDataList[index].krName;
+                    Debug.Log($"changeLanguage.langCDataList[{index}].krName : {changeLanguage.langCDataList[index].krName}");
+                }
+                else if (lang.Equals("EN"))
+                {
+                    fPanels[i].transform.GetChild(j).GetComponent<Text>().text = changeLanguage.langCDataList[index].enName;
+                    Debug.Log($"changeLanguage.langCDataList[{index}].enName : {changeLanguage.langCDataList[index].enName}");
+                }
+            }
+
+        }
+    }
+
+    public void ChangeJournalPanel() // 일지용
+    {
+        for (int i = 0; i < jPanels.Length; i++) //저장된 데이터만큼
+        {
+            int index = changeLanguage.FindPanelNameOfIndex(jPanels[i].name);
+            Debug.Log($"Index : {index}");
+
+            string lang = SettingManager.Instance.GetCurrentLanguageIndexToString();
+            if (lang.Equals("KR"))
+            {
+                jPanels[i].GetComponent<Text>().text = changeLanguage.langCDataList[index].krName;
+                Debug.Log($"changeLanguage.langCDataList[{index}].krName : {changeLanguage.langCDataList[index].krName}");
+            }
+            else if (lang.Equals("EN"))
+            {
+                jPanels[i].GetComponent<Text>().text = changeLanguage.langCDataList[index].enName;
+                Debug.Log($"changeLanguage.langCDataList[{index}].enName : {changeLanguage.langCDataList[index].enName}");
+            }
+        }
+
+    }
+}
