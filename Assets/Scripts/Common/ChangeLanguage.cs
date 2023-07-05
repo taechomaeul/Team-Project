f43995047f9ec0f7c387ae4470dbbb1cb50f632d
Anyeseu Oh <anyeseu2016@gmail.com>
2023-07-05 15:29:46 +0900

    타이틀 씬 다국어 지원 기능 설정
    - 텍스트 연결
    - 설정 확인 버튼(3개 설정 모두)에 ChangeAllPanel 함수 추가

--- Assets/Scripts/Common/ChangeLanguage.cs
+++ Assets/Scripts/Common/ChangeLanguage.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class ChangeLanguage : MonoBehaviour
 {
+    [System.Serializable]
     public class LangCData
     {
         public int index;
@@ -17,19 +18,20 @@ public class ChangeLanguage : MonoBehaviour
     public string langPath;
     private List<Dictionary<string, object>> lang;
     [Header("저장된 언어 데이터 리스트")]
+    [SerializeField]
     public List<LangCData> langCDataList;
 
-    private void Start()
+    private void Awake()
     {
         Scene scene = SceneManager.GetActiveScene();
         if (scene.name.Equals("TitleScene"))
         {
-            StartCoroutine(LoadTitleLangCDataFromCSV());
+            LoadTitleLangCDataFromCSV();
         }
 
         else if(scene.name.Contains("1F") || scene.name.Contains("2F"))
         {
-            StartCoroutine(LoadFloorLangCDataFromCSV());
+            LoadFloorLangCDataFromCSV();
         }
 
     }
@@ -38,7 +40,7 @@ public class ChangeLanguage : MonoBehaviour
     /// CSV 파일을 읽고 내용을 클래스에 저장하는 함수 (1F, 2F 전용)
     /// </summary>
     /// <returns></returns>
-    public IEnumerator LoadFloorLangCDataFromCSV()
+    public void LoadFloorLangCDataFromCSV()
     {
         lang = CSVReader.Read(langPath);
 
@@ -51,39 +53,35 @@ public class ChangeLanguage : MonoBehaviour
                 LangCData newLangCData = new LangCData();
                 newLangCData.index = langCDataList.Count + 1;
                 newLangCData.panelName = lang[i]["Object_Name"].ToString();
-                newLangCData.enName = lang[i]["KR"].ToString();
-                newLangCData.krName = lang[i]["EN"].ToString();
+                newLangCData.krName = lang[i]["KR"].ToString();
+                newLangCData.enName = lang[i]["EN"].ToString();
                 langCDataList.Add(newLangCData);
             }
         }
-
-        yield return new WaitForSeconds(1f);
     }
 
     /// <summary>
     /// CSV 파일을 읽고 내용을 클래스에 저장하는 함수 (Title 전용)
     /// </summary>
     /// <returns></returns>
-    public IEnumerator LoadTitleLangCDataFromCSV()
+    public void LoadTitleLangCDataFromCSV()
     {
         lang = CSVReader.Read(langPath);
 
-        List<LangCData> langCDataList = new List<LangCData>();
+        langCDataList = new List<LangCData>();
 
         for (int i = 0; i < lang.Count; i++)
         {
-            if (lang[i]["Scenes"].Equals("TitleScene")) //타이틀씬일 때
+            if (lang[i]["Language"].Equals("TitleScene")) //타이틀씬일 때
             {
                 LangCData newLangCData = new LangCData();
-                newLangCData.index = langCDataList.Count + 1;
+                newLangCData.index = langCDataList.Count;
                 newLangCData.panelName = lang[i]["Object_Name"].ToString();
-                newLangCData.enName = lang[i]["KR"].ToString();
-                newLangCData.krName = lang[i]["EN"].ToString();
+                newLangCData.krName = lang[i]["KR"].ToString();
+                newLangCData.enName = lang[i]["EN"].ToString();
                 langCDataList.Add(newLangCData);
             }
         }
-
-        yield return new WaitForSeconds(1f);
     }
 
     public int FindPanelNameOfIndex(string panelName)
@@ -93,10 +91,11 @@ public class ChangeLanguage : MonoBehaviour
         {
             if (langCDataList[i].panelName.Equals(panelName))
             {
-                langCDataList[i].index = index;
+                index = langCDataList[i].index;
             }
         }
 
         return index;
     }
+
 }
